afaa1995693c7210779fb8aed095d7b665fd8c51
Rafael Tavares <tavares.rfl@gmail.com>
2025-02-05 20:49:23 -0300

    Participante, Bloco

---
+++ src/main/java/com/devsuperior/desafio_2/entities/Bloco.java
@@ -0,0 +1,68 @@
+package com.devsuperior.desafio_2.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant inicio;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant fim;
+
+    public Bloco() {
+    }
+
+    public Bloco(Integer id, Instant inicio, Instant fim) {
+        this.id = id;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getFim() {
+        return fim;
+    }
+
+    public void setFim(Instant fim) {
+        this.fim = fim;
+    }
+
+    public Instant getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Instant inicio) {
+        this.inicio = inicio;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Bloco bloco = (Bloco) o;
+        return Objects.equals(id, bloco.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/devsuperior/desafio_2/entities/Participante.java
@@ -0,0 +1,66 @@
+package com.devsuperior.desafio_2.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    public Participante() {
+    }
+
+    public Participante(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Participante that = (Participante) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
